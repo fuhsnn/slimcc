fuhsnn/slimcc

commit da6e6053388ff3041d505b437222af1a399d0d97

Remove requirement for labels to be followed by stmt
C23 extension

codegen.c
@@ -1318,7 +1318,6 @@ static void gen_stmt(Node *node) {
     return;
   case ND_CASE:
     println("%s:", node->label);
-    gen_stmt(node->lhs);
     return;
   case ND_BLOCK:
     for (Node *n = node->body; n; n = n->next)
@@ -1335,7 +1334,6 @@ static void gen_stmt(Node *node) {
     return;
   case ND_LABEL:
     println("%s:", node->unique_label);
-    gen_stmt(node->lhs);
     return;
   case ND_RETURN:
     if (node->lhs) {

parse.c
@@ -1649,13 +1649,12 @@ static Node *stmt(Token **rest, Token *tok) {
       ((current_switch->cond->ty->is_unsigned && ((uint64_t)end < begin))))
       error_tok(tok, "empty case range specified");
 
-    tok = skip(tok, ":");
     node->label = new_unique_name();
-    node->lhs = stmt(rest, tok);
     node->begin = begin;
     node->end = end;
     node->case_next = current_switch->case_next;
     current_switch->case_next = node;
+    *rest = skip(tok, ":");
     return node;
   }
 
@@ -1666,10 +1665,9 @@ static Node *stmt(Token **rest, Token *tok) {
       error_tok(tok, "jump crosses VLA initialization");
 
     Node *node = new_node(ND_CASE, tok);
-    tok = skip(tok->next, ":");
     node->label = new_unique_name();
-    node->lhs = stmt(rest, tok);
     current_switch->default_case = node;
+    *rest = skip(tok->next, ":");
     return node;
   }
 
@@ -1777,10 +1775,10 @@ static Node *stmt(Token **rest, Token *tok) {
     Node *node = new_node(ND_LABEL, tok);
     node->label = strndup(tok->loc, tok->len);
     node->unique_label = new_unique_name();
-    node->lhs = stmt(rest, tok->next->next);
     node->goto_next = labels;
     node->top_vla = current_vla;
     labels = node;
+    *rest = tok->next->next;
     return node;
   }
 

test/control2.c
@@ -1,6 +1,24 @@
 #include "test.h"
 #include <stdint.h>
 
+char c23_label(void) {
+  switch (1) {
+    default:
+    case 2:
+  }
+  if (0) {
+    {
+      lab1:
+    }
+    {
+      lab2:
+      int j;
+    }
+    return 77;
+  }
+  goto lab1;
+}
+
 int main(void){
   ASSERT(2, ({ uint32_t i=0; switch(i){case 0 ...0xFFFFFFFF: i=2;} i; }));
   ASSERT(2, ({ int32_t i=0; switch(i){case 0x80000000 ...0x7FFFFFFF: i=2;} i; }));
@@ -11,6 +29,9 @@ int main(void){
   ASSERT(2, ({ int32_t i=-1; switch(i){case -1 ...(int64_t)-1: i=2;} i; }));
 
   ASSERT(2, ({ int64_t i=0; switch(0x123456789){case 0x123456789: i=2;} i; }));
+
+
+  ASSERT(77, c23_label());
 
   printf("OK\n");
 }

commit 6ba8497c18a0486c39c4cb9b36d5ea703cb8881e

Fix label regression by da6e605

codegen.c
@@ -1336,6 +1336,8 @@ static void gen_stmt(Node *node) {
     return;
   case ND_CASE:
     println("%s:", node->label);
+    if (node->lhs)
+      gen_stmt(node->lhs);
     return;
   case ND_BLOCK:
     for (Node *n = node->body; n; n = n->next)
@@ -1352,6 +1354,8 @@ static void gen_stmt(Node *node) {
     return;
   case ND_LABEL:
     println("%s:", node->unique_label);
+    if (node->lhs)
+      gen_stmt(node->lhs);
     return;
   case ND_RETURN:
     if (node->lhs) {

parse.c
@@ -112,7 +112,7 @@ static Initializer *initializer(Token **rest, Token *tok, Type *ty, Type **new_t
 static Node *lvar_initializer(Token **rest, Token *tok, Obj *var);
 static void gvar_initializer(Token **rest, Token *tok, Obj *var);
 static Node *compound_stmt(Token **rest, Token *tok, Node **last);
-static Node *stmt(Token **rest, Token *tok);
+static Node *stmt(Token **rest, Token *tok, bool chained);
 static Node *expr_stmt(Token **rest, Token *tok);
 static Node *expr(Token **rest, Token *tok);
 static int64_t eval(Node *node);
@@ -1547,7 +1547,7 @@ static void loop_body(Token **rest, Token *tok, Node *node) {
   Obj *vla = brk_vla;
   brk_vla = current_vla;
 
-  node->then = stmt(rest, tok);
+  node->then = stmt(rest, tok, true);
 
   brk_label = brk;
   cont_label = cont;
@@ -1569,7 +1569,7 @@ static void loop_body(Token **rest, Token *tok, Node *node) {
 //      | ident ":" stmt
 //      | "{" compound-stmt
 //      | expr-stmt
-static Node *stmt(Token **rest, Token *tok) {
+static Node *stmt(Token **rest, Token *tok, bool chained) {
   if (equal(tok, "return")) {
     Node *node = new_node(ND_RETURN, tok);
     if (consume(rest, tok->next, ";"))
@@ -1592,9 +1592,9 @@ static Node *stmt(Token **rest, Token *tok) {
     tok = skip(tok->next, "(");
     node->cond = expr(&tok, tok);
     tok = skip(tok, ")");
-    node->then = stmt(&tok, tok);
+    node->then = stmt(&tok, tok, true);
     if (equal(tok, "else"))
-      node->els = stmt(&tok, tok->next);
+      node->els = stmt(&tok, tok->next, true);
     *rest = tok;
     return node;
   }
@@ -1614,7 +1614,7 @@ static Node *stmt(Token **rest, Token *tok) {
     Obj *vla = brk_vla;
     brk_vla = current_vla;
 
-    node->then = stmt(rest, tok);
+    node->then = stmt(rest, tok, true);
 
     current_switch = sw;
     brk_label = brk;
@@ -1629,6 +1629,8 @@ static Node *stmt(Token **rest, Token *tok) {
       error_tok(tok, "jump crosses VLA initialization");
 
     Node *node = new_node(ND_CASE, tok);
+    node->label = new_unique_name();
+
     int64_t begin = const_expr(&tok, tok->next);
     int64_t end;
 
@@ -1653,12 +1655,15 @@ static Node *stmt(Token **rest, Token *tok) {
       ((current_switch->cond->ty->is_unsigned && ((uint64_t)end < begin))))
       error_tok(tok, "empty case range specified");
 
-    node->label = new_unique_name();
+    tok = skip(tok, ":");
+    if (chained)
+      node->lhs = stmt(rest, tok, true);
+    else
+      *rest = tok;
     node->begin = begin;
     node->end = end;
     node->case_next = current_switch->case_next;
     current_switch->case_next = node;
-    *rest = skip(tok, ":");
     return node;
   }
 
@@ -1670,8 +1675,13 @@ static Node *stmt(Token **rest, Token *tok) {
 
     Node *node = new_node(ND_CASE, tok);
     node->label = new_unique_name();
+
+    tok = skip(tok->next, ":");
+    if (chained)
+      node->lhs = stmt(rest, tok, true);
+    else
+      *rest = tok;
     current_switch->default_case = node;
-    *rest = skip(tok->next, ":");
     return node;
   }
 
@@ -1778,11 +1788,16 @@ static Node *stmt(Token **rest, Token *tok) {
   if (tok->kind == TK_IDENT && equal(tok->next, ":")) {
     Node *node = new_node(ND_LABEL, tok);
     node->label = strndup(tok->loc, tok->len);
+
+    tok = tok->next->next;
+    if (chained)
+      node->lhs = stmt(rest, tok, true);
+    else
+      *rest = tok;
     node->unique_label = new_unique_name();
     node->goto_next = labels;
     node->top_vla = current_vla;
     labels = node;
-    *rest = tok->next->next;
     return node;
   }
 
@@ -1828,7 +1843,7 @@ static Node *compound_stmt(Token **rest, Token *tok, Node **last) {
         cur = cur->next = new_unary(ND_EXPR_STMT, expr, tok);
       continue;
     }
-    cur = cur->next = stmt(&tok, tok);
+    cur = cur->next = stmt(&tok, tok, false);
   }
 
   if (last)

test/control2.c
@@ -2,6 +2,15 @@
 #include <stdint.h>
 
 char c23_label(void) {
+
+  {
+    int i = 0;
+    if (i != 0)
+lab3:
+      i = 1;
+    ASSERT(0, i);
+  }
+
   switch (1) {
     default:
     case 2: