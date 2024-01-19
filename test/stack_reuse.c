a5215361a00cbb8895744580045e63deb97d91eb
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2024-01-19 18:36:39 +0800

    Extend stack-reuse test

--- test/stack_reuse.c
+++ test/stack_reuse.c
@@ -13,14 +13,39 @@ int main(void) {
   ASSERT(3, ({ S1 s = {1,2,3,4}; s; }).i[ ({ S1 s = {}; 2;}) ] );
   ASSERT(4, ({ S1 s = {1,2,3,4}; s; }).i[ ({ S1 s = {}; 3;}) ] );
 
-  ASSERT(1, sel(1, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[0]);
-  ASSERT(2, sel(1, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[1]);
-  ASSERT(3, sel(1, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[2]);
-  ASSERT(4, sel(1, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[3]);
-  ASSERT(5, sel(0, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[0]);
-  ASSERT(6, sel(0, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[1]);
-  ASSERT(7, sel(0, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[2]);
-  ASSERT(8, sel(0, pass(&(S1){1,2,3,4}), pass(&(S1){5,6,7,8}))->i[3]);
+  {
+    S1 sA={1,2,3,4};
+    S1 sB={5,6,7,8};
+
+    ASSERT(1, sel(1, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[0]);
+    ASSERT(2, sel(1, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[1]);
+    ASSERT(3, sel(1, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[2]);
+    ASSERT(4, sel(1, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[3]);
+    ASSERT(5, sel(0, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[0]);
+    ASSERT(6, sel(0, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[1]);
+    ASSERT(7, sel(0, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[2]);
+    ASSERT(8, sel(0, pass(&(S1[]){sA}), pass(&(S1[]){sB}))->i[3]);
+
+    ASSERT(1, sel(1,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[0]);
+    ASSERT(2, sel(1,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[1]);
+    ASSERT(3, sel(1,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[2]);
+    ASSERT(4, sel(1,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[3]);
+    ASSERT(5, sel(0,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[0]);
+    ASSERT(6, sel(0,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[1]);
+    ASSERT(7, sel(0,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[2]);
+    ASSERT(8, sel(0,({S1*p=alloca(sizeof(S1));*p=sA;p;}),({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[3]);
+
+    ASSERT(1, sel(1, pass(&(S1[]){sA}), ({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[0]);
+    ASSERT(2, sel(1, pass(&(S1[]){sA}), ({S1*p=alloca(sizeof(S1));*p=sB;p;}))->i[1]);
+    ASSERT(3, sel(1, ({S1*p=alloca(sizeof(S1));*p=sA;p;}), pass(&(S1[]){sB}))->i[2]);
+    ASSERT(4, sel(1, ({S1*p=alloca(sizeof(S1));*p=sA;p;}), pass(&(S1[]){sB}))->i[3]);
+    ASSERT(5, sel(0, pass(&(S1[]){sA}), pass(&(S1[]){({S1*p=alloca(sizeof(S1));*p=sB;*p;})}))->i[0]);
+    ASSERT(6, sel(0, pass(&(S1[]){sA}), pass(&(S1[]){({S1*p=alloca(sizeof(S1));*p=sB;*p;})}))->i[1]);
+    ASSERT(7, sel(0, pass(&(S1[]){({S1*p=alloca(sizeof(S1));*p=sA;*p;})}), pass(&(S1[]){sB}))->i[2]);
+    ASSERT(8, sel(0, pass(&(S1[]){({S1*p=alloca(sizeof(S1));*p=sA;*p;})}), pass(&(S1[]){sB}))->i[3]);
+
+  }
+
 
   printf("OK\n");
 }
