1b579aa4097e0b1ef3968ad62db1f462e083fa35
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2024-11-26 19:26:40 +0800

    Remove old thirdparty scripts

--- test/thirdparty/tinycc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-repo='git@github.com:TinyCC/tinycc.git'
-. test/thirdparty/common
-git reset --hard df67d8617b7d1d03a480a28f9f901848ffbfb7ec
-
-./configure --cc=$chibicc
-$make clean
-$make
-$make CC=cc test
