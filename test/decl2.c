3dc1a3f4f90263bfd878e3e10f8f4e515f04eba3
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-03-26 18:09:44 +0800

    Move test cases around

--- test/decl2.c
+++ test/decl2.c
@@ -1,13 +1,8 @@
 #include "test.h"
 
-long double ld = 0.0L;
-
 #define CAT(x) b##x
 int CAT(6_) = 7;
 int CAT(8µ) = 9;
-
-const int arr[2][2] = {{1,2},{3,4}};
-int *const p1 = arr[1];
 
 int fn(float), var1 = 7, fn2(void), var2 = 11;
 
@@ -21,40 +16,12 @@ extern int init_extern = 3;
 static inline int static_inline_fn(void);
 int (*static_inline_fn_ptr)(void) = static_inline_fn;
 
-int g1 = ((int){77});
-int g2 = arr[1][1];
-
-const struct {
-  int *a, *b, *c;
-} relo = {.a = &g1, .c = &g2};
-int *p2 = relo.c;
-int *p1p = p1;
-
-struct S { int i, j, k; };
-struct S const s1 = {99, 88, 77};
-struct S s2 = (struct S){33,44,55};
-struct S s3 = s1;
-#ifdef NOTGCC
-int g3 = (struct S){55,66,77}.j;
-int g4 = (const int[]){11,22,33,44}[2];
-#endif
-
-static long long obar = 2 + (long long)"foobar";
-
-_Bool b = 2;
-_Bool ba[] = {3,3,3};
-struct {
-    _Bool b;
-} bs = {4};
-
 int main(void) {
   {
     ASSERT(7, b6_);
     ASSERT(9, b8µ);
   }
-  {
-    ASSERT(3, *p1);
-  }
+
   {
     ASSERT(7, var1);
     ASSERT(11, var2);
@@ -75,30 +42,6 @@ int main(void) {
   ASSERT(3, init_extern);
 
   ASSERT(127, static_inline_fn_ptr());
-
-  ASSERT(77, g1);
-  ASSERT(4, g2);
-  ASSERT(1, p2 == &g2);
-  ASSERT(1, p1p == p1);
-
-  ASSERT(33, s2.i);
-  ASSERT(44, s2.j);
-  ASSERT(55, s2.k);
-
-  ASSERT(99, s3.i);
-  ASSERT(88, s3.j);
-  ASSERT(77, s3.k);
-
-#ifdef NOTGCC
-  ASSERT(66, g3);
-  ASSERT(33, g4);
-#endif
-
-  ASSERT(1, b);
-  ASSERT(1, ba[2]);
-  ASSERT(1, bs.b);
-
-  ASSERT(0, strcmp("obar", (char *)obar));
 
   ASSERT(0, ({ struct S {}; sizeof(struct S);}));
   ASSERT(0, ({ union U {}; sizeof(union U);}));
--- test/initializer2.c
+++ test/initializer2.c
@@ -1,6 +1,63 @@
 #include "test.h"
 
+long double ld = 0.0L;
+
+const int arr[2][2] = {{1,2},{3,4}};
+int *const p1 = arr[1];
+
+int g1 = ((int){77});
+int g2 = arr[1][1];
+
+const struct {
+  int *a, *b, *c;
+} relo = {.a = &g1, .c = &g2};
+int *p2 = relo.c;
+int *p1p = p1;
+
+struct S { int i, j, k; };
+struct S const s1 = {99, 88, 77};
+struct S s2 = (struct S){33,44,55};
+struct S s3 = s1;
+#ifdef NOTGCC
+int g3 = (struct S){55,66,77}.j;
+int g4 = (const int[]){11,22,33,44}[2];
+#endif
+
+static long long obar = 2 + (long long)"foobar";
+
+_Bool b = 2;
+_Bool ba[] = {3,3,3};
+struct {
+    _Bool b;
+} bs = {4};
+
 int main(void) {
+  ASSERT(3, *p1);
+
+  ASSERT(77, g1);
+  ASSERT(4, g2);
+  ASSERT(1, p2 == &g2);
+  ASSERT(1, p1p == p1);
+
+  ASSERT(33, s2.i);
+  ASSERT(44, s2.j);
+  ASSERT(55, s2.k);
+
+  ASSERT(99, s3.i);
+  ASSERT(88, s3.j);
+  ASSERT(77, s3.k);
+
+#ifdef NOTGCC
+  ASSERT(66, g3);
+  ASSERT(33, g4);
+#endif
+
+  ASSERT(1, b);
+  ASSERT(1, ba[2]);
+  ASSERT(1, bs.b);
+
+  ASSERT(0, strcmp("obar", (char *)obar));
+
   {
     int i[2][4] = {[0][0 ... 1] = 4, 2, 3, 7, [1][1 ... 2] = 1, 9};
     int ans[] = {4,4,2,3,7,1,1,9};
