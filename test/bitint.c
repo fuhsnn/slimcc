87999b53027fa0157fee6302c34ada102148d657
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-04-05 23:38:36 +0800

    Implement C23 _BitInt: type conversion

--- test/bitint.c
+++ test/bitint.c
@@ -15,6 +15,44 @@ SASSERT(sizeof(_BitInt(15)[17]) == 34);
 SASSERT(sizeof(_BitInt(31)[17]) == 68);
 SASSERT(sizeof(_BitInt(63)[17]) == 136);
 
+#define CHK_USUAL_CONV(A, B, C) \
+  SASSERT(_Generic((A)0+(B)0, C:1))
+
+CHK_USUAL_CONV(_BitInt(2) signed, _BitInt(1) unsigned, _BitInt(2));
+CHK_USUAL_CONV(_BitInt(2), unsigned _BitInt(2), unsigned _BitInt(2));
+
+CHK_USUAL_CONV(_BitInt(2), _Bool, int);
+CHK_USUAL_CONV(unsigned _BitInt(1), _Bool, int);
+
+#define W (__SIZEOF_INT__ * 8)
+
+CHK_USUAL_CONV(signed _BitInt(W-1), signed, signed);
+CHK_USUAL_CONV(signed _BitInt(W-1), unsigned, unsigned);
+CHK_USUAL_CONV(unsigned _BitInt(W-1), signed, signed);
+CHK_USUAL_CONV(unsigned _BitInt(W-1), unsigned, unsigned);
+
+CHK_USUAL_CONV(signed _BitInt(W), signed, signed);
+CHK_USUAL_CONV(signed _BitInt(W), unsigned, unsigned);
+CHK_USUAL_CONV(unsigned _BitInt(W), signed, unsigned);
+CHK_USUAL_CONV(unsigned _BitInt(W), unsigned, unsigned);
+
+CHK_USUAL_CONV(signed _BitInt(W+1), signed, signed _BitInt(W+1));
+CHK_USUAL_CONV(signed _BitInt(W+1), unsigned, signed _BitInt(W+1));
+CHK_USUAL_CONV(unsigned _BitInt(W+1), signed, unsigned _BitInt(W+1));
+CHK_USUAL_CONV(unsigned _BitInt(W+1), unsigned, unsigned _BitInt(W+1));
+
+void bitint_bitfiled(void) {
+  ASSERT(1, ({struct { _BitInt(W-1)i:W-1;} s; _Generic(-s.i, _BitInt(W-1):1);}));
+  ASSERT(1, ({struct { _BitInt( W )i:W-1;} s; _Generic(-s.i, _BitInt( W ):1);}));
+  ASSERT(1, ({struct { _BitInt(W+1)i:W-1;} s; _Generic(-s.i, _BitInt(W+1):1);}));
+
+  ASSERT(1, ({struct { _BitInt(W-1)i:W-1;} s; _Generic(0+s.i, int:1);}));
+  ASSERT(1, ({struct { _BitInt( W )i:W-1;} s; _Generic(0+s.i, int:1);}));
+  ASSERT(1, ({struct { _BitInt(W+1)i:W-1;} s; _Generic(0+s.i, _BitInt(W+1):1);}));
+}
+
 int main() {
+  bitint_bitfiled();
+
   printf("OK\n");
 }
