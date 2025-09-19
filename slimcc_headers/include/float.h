2849b3549a4314859a10543bc1425e62471f5a56
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-09-20 03:12:26 +0800

    Rework <float.h> according to std version, add C23 bits

--- slimcc_headers/include/float.h
+++ slimcc_headers/include/float.h
@@ -2,7 +2,6 @@
 #define __STDFLOAT_H
 
 #define DECIMAL_DIG 21
-#define FLT_EVAL_METHOD 0 // C11 5.2.4.2.2p9
 #define FLT_RADIX 2
 
 #if defined(__has_include)
@@ -35,7 +34,6 @@
 #define FLT_MIN 0x1p-126f
 #define FLT_MIN_10_EXP (-37)
 #define FLT_MIN_EXP (-125)
-#define FLT_TRUE_MIN 0x1p-149f
 
 #define DBL_DIG 15
 #define DBL_EPSILON 0x1p-52
@@ -46,7 +44,6 @@
 #define DBL_MIN 0x1p-1022
 #define DBL_MIN_10_EXP (-307)
 #define DBL_MIN_EXP (-1021)
-#define DBL_TRUE_MIN 0x1p-1074
 
 #define LDBL_DIG 18
 #define LDBL_EPSILON 0x1p-63L
@@ -57,6 +54,35 @@
 #define LDBL_MIN 0x1p-16382L
 #define LDBL_MIN_10_EXP (-4931)
 #define LDBL_MIN_EXP (-16381)
+
+#if __STDC_VERSION__ >= 199901L
+#define FLT_EVAL_METHOD 0
+#endif
+
+#if __STDC_VERSION__ >= 201112L
+#define FLT_HAS_SUBNORM 1
+#define FLT_DECIMAL_DIG 9
+#define FLT_TRUE_MIN 0x1p-149f
+
+#define DBL_HAS_SUBNORM 1
+#define DBL_DECIMAL_DIG 17
+#define DBL_TRUE_MIN 0x1p-1074
+
+#define LDBL_HAS_SUBNORM 1
+#define LDBL_DECIMAL_DIG 21
 #define LDBL_TRUE_MIN 0x1p-16445L
+#endif
+
+#if __STDC_VERSION__ >= 202311L
+#define __STDC_VERSION_FLOAT_H__ 202311L
+
+#define FLT_IS_IEC_60559 1
+#define DBL_IS_IEC_60559 1
+#define LDBL_IS_IEC_60559 1
+
+#define FLT_NORM_MAX FLT_MAX
+#define DBL_NORM_MAX DBL_MAX
+#define LDBL_NORM_MAX LDBL_MAX
+#endif
 
 #endif
