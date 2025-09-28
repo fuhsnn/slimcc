168fb3f40a63cbcfff898078e2db114be14a37e5
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2025-09-28 18:56:29 +0800

    Add uclibc-ng platform

---
+++ platform/linux-uclibcng-bootstrap.c
@@ -0,0 +1,33 @@
+
+#include "slimcc.h"
+
+void platform_init(void) {
+  define_macro("__ELF__", "1");
+
+  define_macro("__linux", "1");
+  define_macro("__linux__", "1");
+
+  init_ty_lp64();
+
+  define_macro("__STDC_NO_THREADS__", "1");
+}
+
+void platform_stdinc_paths(StringArray *paths) {
+  add_include_path(paths, ROOT_DIR"/lib/slimcc/include");
+  add_include_path(paths, ROOT_DIR"/usr/include");
+}
+
+void platform_search_dirs(StringArray *paths) {
+  strarray_push(paths, ROOT_DIR"/usr/lib");
+}
+
+void run_assembler(StringArray *as_args, char *input, char *output) {
+  run_assembler_gnustyle(as_args, input, output);
+}
+
+void run_linker(StringArray *paths, StringArray *inputs, char *output) {
+  run_linker_gnustyle(paths, inputs, output,
+    ROOT_DIR"/lib/ld64-uClibc.so.1",
+    ROOT_DIR"/usr/lib",
+    NULL);
+}
