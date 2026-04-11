ebe41e53a4d05fba0e0565527c06a06e61e7696e
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2026-04-11 08:41:30 +0800

    Don't define va_copy() under C89, add __va_copy

--- slimcc_headers/include/stdarg.h
+++ slimcc_headers/include/stdarg.h
@@ -11,11 +11,15 @@ typedef __builtin_va_list va_list;
 #define va_start(ap, last) __builtin_va_start(ap, last)
 #endif
 
+#if __STDC_VERSION__ >= 199901L
+#define va_copy(dest, src) __builtin_va_copy(dest, src)
+#endif
+
+#define __va_copy(dest, src) __builtin_va_copy(dest, src)
+
 #define va_end(ap) __builtin_va_end(ap)
 
 #define va_arg(ap, ty) __builtin_va_arg(ap, ty)
-
-#define va_copy(dest, src) __builtin_va_copy(dest, src)
 
 #define __GNUC_VA_LIST 1
 typedef va_list __gnuc_va_list;
