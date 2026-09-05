fa8370f2dee1260efa243949de0286eea4449341
fuhsnn <66062782+fuhsnn@users.noreply.github.com>
2026-09-05 08:22:01 +0800

    Check assign-ability of structs with const member

---
+++ test/assign.c
@@ -0,0 +1,24 @@
+#include "test.h"
+
+int main(void) {
+  {
+    struct { int i; } s1, s2;
+    //SREJ (s1.i ? s1 : s2).i = 13;
+    //SREJ (s1 = s2).i = 13;
+    //SREJ (1, s2).i = 13;
+    ({ s2; }).i = 13;
+    //SREJ ((struct S)s2).i = 13;
+  }
+  {
+    struct S { const int i; } a1, b1 = {};
+    //SREJ a1 = b1;
+    struct { struct { const int i; }; int j; } a2, b2 = {};
+    //SREJ a2 = b2;
+    struct { struct { const int a[5]; } sa[3]; } a3, b3 = {};
+    //SREJ a3 = b3;
+    struct { struct S s; } a4, b4 = {};
+    //SREJ a4 = b4;
+  }
+
+  puts("OK");
+}
